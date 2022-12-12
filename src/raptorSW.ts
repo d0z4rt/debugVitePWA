c9f25924b346903caa91bc39e282f08d2a771de0
d0z4rt <d0z@d0z.eu>
2022-12-12 17:09:10 +0100

    fix: clean Output after Precache

--- src/raptorSW.ts
+++ src/raptorSW.ts
@@ -26,11 +26,11 @@ self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
 })
 
-// clean old assets
-cleanupOutdatedCaches()
-
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
+
+// clean old assets
+cleanupOutdatedCaches()
 
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
